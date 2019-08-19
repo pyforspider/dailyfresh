7200389885cb9872a81b606f03d8dc1de60cdc10
shaojian <1303292392@qq.com>
2019-08-19 22:27:51 +0800

    a

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gf*%pnf=1kl*9yr&84oj6=s+#rtg50yu!#%ti%#yvfr*w_9h)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
