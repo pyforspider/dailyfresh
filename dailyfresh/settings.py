045f35127159082218e2574a8dff4861cd8f41d0
shaojian <1303292392@qq.com>
2019-08-19 22:20:47 +0800

    .

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -179,7 +179,7 @@ LOGIN_URL = '/user/login'
 HAYSTACK_CONNECTIONS = {
 	'default': {
 		# 使用whoosh引擎
-		'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+		'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
 		# 索引文件路径
 		'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
 	}
